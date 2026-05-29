2e569b3d44b6a7fcd42dad76638d72177c92811c
ergoman-ia <ergoman@kakao.com>
2026-05-29 18:02:31 +0900

    Add files via upload

---
+++ secrets.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+            apiKey: "AIzaSyC2oyeiuBxNDGGPExC8Y32w8OfJ4wODbp4",
+            authDomain: "angle-fa9e3.firebaseapp.com",
+            projectId: "angle-fa9e3",
+            storageBucket: "angle-fa9e3.firebasestorage.app",
+            messagingSenderId: "560938337056",
+            appId: "1:560938337056:web:72930d60a2b271903ecd45"
+        };
